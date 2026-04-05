01a514680606e82c7ac702200b5ac6b82c93674d
Martin Olsson <info@martinolsson.com>
2026-04-05 14:16:30 +0200

    Improve usability of the Windows upgrade script

--- Scripts/Start-Windows11Upgrade.ps1
+++ Scripts/Start-Windows11Upgrade.ps1
@@ -62,6 +62,50 @@ function Get-ScriptVersion {
     return $currentScriptVersion
 }
 
+function Get-OneDriveSyncState {
+    $syncDiagnosticsFilePath = Join-Path -Path $env:LOCALAPPDATA -ChildPath '\Microsoft\OneDrive\logs\Business1\SyncDiagnostics.log'
+    $stateValue = $null
+
+    if (Test-Path -Path $syncDiagnosticsFilePath) {
+        $progressState = Get-Content -Path $syncDiagnosticsFilePath -ErrorAction SilentlyContinue | Where-Object { $_.Contains("SyncProgressState") } | ForEach-Object { -split $_ | Select-Object -Index 1 }
+        if ($progressState) {
+            switch ($progressState){
+                0 { $stateValue = "Healthy" }
+                10 { $stateValue = "File merge conflict" }
+                42{ $stateValue = "Healthy" }
+                256 { $stateValue = "File locked" }
+                258 { $stateValue = "File merge conflict" }
+                8456 { $stateValue = "You don't have permission to sync this library" }
+                16777216 { $stateValue = "Healthy" }
+                12544 { $stateValue = "Healthy" }
+                65536 { $stateValue = "Paused" }
+                32786 { $stateValue = "File merge conflict" }
+                4106 { $stateValue = "File merge conflict" }
+                20480 { $stateValue = "File merge conflict" }
+                24576 { $stateValue = "File merge conflict" }
+                25088 { $stateValue = "File merge conflict" }
+                8449 { $stateValue = "File locked" }
+                8194 { $stateValue = "Disabled" }
+                1854 { $stateValue = "Unhealthy" }
+                12290 { $stateValue = "Access permission" }
+                default { $stateValue = "Unknown: $progressState" }
+            }
+        }
+        else {
+            $stateValue = 'Invalid sync state'
+        }
+
+        if ((Get-Item -Path $syncDiagnosticsFilePath).LastWriteTime -le (Get-Date).Date.AddDays(-1)) {
+            $stateValue = 'Not recently synced'
+        }
+    }
+    else {
+        $stateValue = 'No sync state'
+    }
+
+    return $stateValue
+}
+
 function Out-LogFile {
     [CmdletBinding(SupportsShouldProcess)]
     param(
@@ -85,6 +129,14 @@ function Out-LogFile {
 
     ('[{0} | v{1}] {2}' -f (Get-Date -Format 'yyyy-MM-dd HH:mm:ss'), $ScriptVersion, $Content) | Out-File -FilePath $FilePath -Append -ErrorAction Continue
 }
+
+Write-Host '---------------------' -ForegroundColor Cyan
+Write-Host 'UPGRADE TO WINDOWS 11' -ForegroundColor Cyan
+Write-Host '---------------------' -ForegroundColor Cyan
+
+Write-Host "`nThis script will attempt to upgrade your system to Windows 11." -ForegroundColor Yellow
+Write-Host 'The upgrade should take around 30-60 minutes.' -ForegroundColor Yellow
+Read-Host 'Press <Enter> to start the upgrade or <Ctrl+C> to cancel'
 
 $systemDrive = $env:SystemDrive
 if ([string]::IsNullOrEmpty($systemDrive)) {
@@ -126,28 +178,21 @@ if ($osName -match 'Windows 10') {
 
 # Only upgrade if the system is eligible for upgrade (or if using the Force parameter).
 if ($isSystemEligibleForUpgrade -or $Force) {
-    if ((Get-Command -Name 'Confirm-SecureBootUEFI' -ErrorAction SilentlyContinue | Measure-Object).Count -gt 0) {
-        try {
-            if (-not (Confirm-SecureBootUEFI -ErrorAction SilentlyContinue)) {
-                Write-Warning "Secure Boot is disabled. It's recommended (but not required) to enable it in the BIOS settings."
-                Out-LogFile @logParams -Content "Secure Boot is disabled. It's recommended (but not required) to enable it in the BIOS settings."
-            }
-        }
-        catch {
-            Write-Warning 'Unable to validate if Secure Boot is enabled. The upgrade can proceed anyway.'
-        }
-    }
-
+    # Check if TPM is active.
+    Write-Host "`nChecking TPM..."
     $tpm = Get-WmiObject -Namespace 'Root\CIMv2\Security\MicrosoftTpm' -Class Win32_Tpm
     if (($null -eq $tpm) -or (-not $tpm.IsEnabled_InitialValue) -or (-not $tpm.IsActivated_InitialValue)) {
         Out-LogFile @logParams -Content 'TPM is not enabled (or not activated). Enable/activate the TPM in the BIOS settings to be able to upgrade.'
         throw 'TPM is not enabled (or not activated). Enable/activate the TPM in the BIOS settings to be able to upgrade.'
     }
+    else {
+        Write-Host 'TPM is enabled.' -ForegroundColor Green
+    }
 
     # Allow upgrade on older devices.
     $tpmMajorVersion = $tpm.SpecVersion.Split(',')[0] -as [int]
     if ($tpmMajorVersion -lt 2) {
-        Write-Warning 'The script determined that this is an older device but will try to allow the upgrade anyway.'
+        Write-Verbose 'The script determined that this is an older device but will try to allow the upgrade anyway.'
         try {
             $registrySystemSetupPath = 'HKLM:\SYSTEM\Setup\MoSetup'
             if (-not (Test-Path -Path $registrySystemSetupPath)) {
@@ -159,12 +204,12 @@ if ($isSystemEligibleForUpgrade -or $Force) {
                 $registryAllowUpgradeName = 'AllowUpgradesWithUnsupportedTPMOrCPU'
                 if ($null -eq (Get-ItemProperty -Path $registrySystemSetupPath -Name $registryAllowUpgradeName -ErrorAction SilentlyContinue)) {
                     Write-Verbose 'Creating system setup registry value...'
-                    New-ItemProperty -Path $registrySystemSetupPath -Name $registryAllowUpgradeName -PropertyType 'DWORD' -Value 1 -ErrorAction Stop
+                    New-ItemProperty -Path $registrySystemSetupPath -Name $registryAllowUpgradeName -PropertyType 'DWORD' -Value 1 -ErrorAction Stop | Out-Null
                     Out-LogFile @logParams -Content 'Added value to the registry to allow upgrade on an older device.'
                 }
                 elseif ((Get-ItemProperty -Path $registrySystemSetupPath -Name $registryAllowUpgradeName -ErrorAction SilentlyContinue).($registryAllowUpgradeName) -ne 1) {
                     Write-Verbose 'Updating existing system setup registry value...'
-                    Set-ItemProperty -Path $registrySystemSetupPath -Name $registryAllowUpgradeName -Value 1 -ErrorAction Stop
+                    Set-ItemProperty -Path $registrySystemSetupPath -Name $registryAllowUpgradeName -Value 1 -ErrorAction Stop | Out-Null
                     Out-LogFile @logParams -Content 'Updated value in the registry to allow upgrade on an older device.'
                 }
             }
@@ -177,6 +222,68 @@ if ($isSystemEligibleForUpgrade -or $Force) {
             Write-Error "Failed to configure the registry to allow upgrade on an older device. $PSItem"
             Out-LogFile @logParams -Content "Failed to configure the registry to allow upgrade on an older device. $PSItem"
         }
+    }
+
+    # Check free storage space.
+    Write-Host "`nChecking available disk space..."
+    $properties = @(
+        @{ Name = 'FreeSpaceGB'; Expression = { [float]($_.FreeSpace / 1GB) } }
+    )
+    $driveType = 3
+    $diskInfo = Get-WmiObject -Class Win32_LogicalDisk -ComputerName LOCALHOST | Where-Object { ($_.DriveType -eq $driveType) -and ($_.DeviceID -eq $systemDrive) } | Select-Object -Property $properties
+    $requiredDiskSpace = 64
+    if ($diskInfo.FreeSpaceGB -lt $requiredDiskSpace) {
+        $freeDiskSpaceRounded = [Math]::Floor($diskInfo.FreeSpaceGB)
+        Out-LogFile @logParams -Content "Windows requires at least $requiredDiskSpace GB free disk space to be able to upgrade. Currently available: $freeDiskSpaceRounded GB"
+        throw "Windows requires at least $requiredDiskSpace GB free disk space to be able to upgrade. Currently available: $freeDiskSpaceRounded GB"
+    }
+    else {
+        Write-Host 'Disk space is OK.' -ForegroundColor Green
+    }
+
+    # Check OneDrive sync state.
+    Write-Host "`nChecking OneDrive sync status..."
+    $oneDriveSyncState = Get-OneDriveSyncState
+    if ($oneDriveSyncState -ne 'Healthy') {
+        Write-Host "OneDrive sync state is degraded. Status from diagnostics log: $oneDriveSyncState" -ForegroundColor Red
+        Write-Host 'Cancel the script, verify that OneDrive is enabled and synchronized, then start the script again.' -ForegroundColor Yellow
+        Out-LogFile @logParams -Content "OneDrive sync state is degraded. Status from diagnostics log: $oneDriveSyncState"
+        Read-Host 'Press <Enter> to proceed with the upgrade anyway or <Ctrl+C> to cancel'
+    }
+    else {
+        Write-Host 'OneDrive is synced.' -ForegroundColor Green
+    }
+
+    # Check if secure boot is enabled.
+    Write-Host "`nChecking Secure Boot..."
+    if ((Get-Command -Name 'Confirm-SecureBootUEFI' -ErrorAction SilentlyContinue | Measure-Object).Count -gt 0) {
+        try {
+            if (-not (Confirm-SecureBootUEFI -ErrorAction SilentlyContinue)) {
+                Write-Host "Secure Boot is disabled. It's recommended (but not required) to enable it in the BIOS settings. The upgrade will proceed anyway." -ForegroundColor Yellow
+                Out-LogFile @logParams -Content "Secure Boot is disabled. It's recommended (but not required) to enable it in the BIOS settings."
+            }
+            else {
+                Write-Host 'Secure Boot is enabled.' -ForegroundColor Green
+            }
+        }
+        catch {
+            Write-Host 'Unable to validate if Secure Boot is enabled. The upgrade will proceed anyway.' -ForegroundColor Yellow
+        }
+    }
+    else {
+        Write-Host 'Unable to verify Secure Boot state. The upgrade will proceed anyway.' -ForegroundColor Yellow
+    }
+
+    $registryWindowsUpdatePath = 'HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate'
+    if (Test-Path -Path $registryWindowsUpdatePath) {
+        Write-Verbose 'Removing Windows Update release version registry values...'
+        Remove-ItemProperty -Path $registryWindowsUpdatePath -Name @('TargetReleaseVersion', 'TargetReleaseVersionInfo') -Force -ErrorAction SilentlyContinue | Out-Null
+    }
+
+    $registryWindowsUpdateUxSettingsPath = 'HKLM:\SOFTWARE\Microsoft\WindowsUpdate\UX\Settings'
+    if (Test-Path -Path $registryWindowsUpdateUxSettingsPath) {
+        Write-Verbose 'Removing Windows Update offer declined registry value...'
+        Remove-ItemProperty -Path $registryWindowsUpdateUxSettingsPath -Name 'SvOfferDeclined' -Force -ErrorAction SilentlyContinue | Out-Null
     }
 
     # Stop any currently running Windows upgrade processes.
@@ -196,20 +303,38 @@ if ($isSystemEligibleForUpgrade -or $Force) {
         }
         catch {
             Write-Error "Failed to download the Windows 11 installation assistant. $PSItem"
-            Out-LogFile @logParams -Content "Failed to downloaded the Windows 11 installation assistant. $PSItem"
+            Out-LogFile @logParams -Content "Failed to download the Windows 11 installation assistant. $PSItem"
         }
+    }
+
+    if (-not (Test-Path -Path $installerFilePath -PathType Leaf)) {
+        throw "Unable to find the Windows 11 installation assistant file: $installerFilePath"
     }
 
     if ($PSCmdlet.ShouldProcess("File: $installerFilePath", 'Start Windows 11 upgrade')) {
         try {
-            Write-Verbose ('Started the Windows 11 upgrade at {0}. The upgrade will take approximately 30-60 minutes.' -f (Get-Date -Format 'HH:mm'))
-            Write-Verbose 'The computer will automatically reboot when the upgrade is finished.'
-            Write-Verbose 'Installing Windows 11...'
+            $startDateTime = Get-Date
+            Write-Host "`nStarted the Windows 11 upgrade at $($startDateTime.ToShortTimeString())." -ForegroundColor Green
+            Write-Host 'The computer will automatically reboot when the upgrade has finished.' -ForegroundColor Yellow
+            Write-Host 'Installing Windows 11...'
             Out-LogFile @logParams -Content 'Started the Windows 11 upgrade.'
-
             Start-Process -FilePath $installerFilePath -ArgumentList @('/QuietInstall /SkipEULA /Auto Upgrade /NoRestartUI /CopyLogs {0}' -f $tempDirectoryPath) -Wait
-            Write-Verbose 'Exited the upgrade process (the script cannot know if the upgrade was successful or not).'
-            Out-LogFile @logParams -Content 'Exited the upgrade process (the script cannot know if the upgrade was successful or not).'
+
+            $stopDateTime = Get-Date
+            $upgradeTotalMinutes = ($stopDateTime - $startDateTime).TotalMinutes
+            $upgradeTotalMinutesRounded = [Math]::Round($upgradeTotalMinutes)
+            Write-Host "Exited the upgrade process at $($stopDateTime.ToShortTimeString())."
+            Out-LogFile @logParams -Content 'Exited the upgrade process.'
+
+            if ($upgradeTotalMinutes -lt 15) {
+                Write-Host "The upgrade has probably failed. The process time was short (~$upgradeTotalMinutesRounded minutes)." -ForegroundColor Red
+                Out-LogFile @logParams -Content "The upgrade has probably failed. The process time was short (~$upgradeTotalMinutesRounded) minutes)."
+            }
+            else {
+                Start-Sleep -Seconds 60
+                Write-Host "The upgrade has probably failed. The computer should have restarted itself by now." -ForegroundColor Red
+                Out-LogFile @logParams -Content 'The upgrade has probably failed. The computer should have restarted itself by now.'
+            }
         }
         catch {
             Out-LogFile @logParams -Content "The Windows 11 upgrade failed. $PSItem"
